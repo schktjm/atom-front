59e40cf40a34ab147b540ceddd25552229c8d73c
schktjm <si2.o.mjt@gmail.com>
2019-09-10 14:28:03 +0900

    set css flamework and theme

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import Element from 'element-ui'
+import './element-variables.scss'
 
+Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
