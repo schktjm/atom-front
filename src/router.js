c3c33dc9bfd49a107821bfafbc51d14cf3218fb1
schktjm <si2.o.mjt@gmail.com>
2019-09-13 16:51:10 +0900

    make list

--- src/router.js
+++ src/router.js
@@ -2,19 +2,29 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/list',
-      name : 'RouteList',
-      component: () => import('./views/RouteList')
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/list',
+            name: 'RouteList',
+            component: () => import('./views/RouteList')
+        },
+        {
+            path: '/detail/:id',
+            name: 'RouteDetail',
+            component: () => import('./views/RouteDetail')
+        },
+        {
+            path: '/test',
+            name: 'test',
+            component: () => import('./components/MoveCard')
+        }
+    ]
 })
---
+++ src/views/RouteDetail.vue
@@ -0,0 +1,41 @@
+<template>
+  <div style="height: 300px;">
+    <div>{{data}}</div>
+    <el-timeline>
+      <el-timeline-item timestamp="2018/4/12" placement="top">
+        <el-card>
+          <h4>Update Github template</h4>
+          <p>Tom committed 2018/4/12 20:46</p>
+        </el-card>
+      </el-timeline-item>
+      <el-timeline-item timestamp="2018/4/3" placement="top">
+        <el-card>
+          <h4>Update Github template</h4>
+          <p>Tom committed 2018/4/3 20:46</p>
+        </el-card>
+      </el-timeline-item>
+      <el-timeline-item timestamp="2018/4/2" placement="top">
+        aizu wakamatsu station
+        <el-card>
+          <h4>Update Github template</h4>
+          <p>Tom committed 2018/4/2 20:46</p>
+        </el-card>
+      </el-timeline-item>
+      <el-timeline-item timestamp="2019/9/12">aaa</el-timeline-item>
+    </el-timeline>
+  </div>
+</template>
+
+<script>
+
+    import data from '../assets/json/routeDetail';
+
+    export default {
+        name: 'RouteDetail',
+        data() {
+            return {
+                data: data
+            }
+        }
+    }
+</script>
